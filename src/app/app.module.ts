b96f0207e95b856efefe67567a0e9b1edb7cbdc5
mahdi chekini <me.chekini@gmail.com>
2022-08-17 20:40:19 +0200

    Add Jenkinsfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import {FeaturesModule} from "./features/features.module";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpRequestInterceptor} from "./core/interceptors/http.request.interceptor";
+import {OAuthModule} from "angular-oauth2-oidc";
 
 @NgModule({
   declarations: [
@@ -10,9 +15,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    HttpClientModule,
+    FeaturesModule,
+    AppRoutingModule,
+    OAuthModule.forRoot({resourceServer: {allowedUrls: ['/product-api', '/order-api'], sendAccessToken: true}})
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
